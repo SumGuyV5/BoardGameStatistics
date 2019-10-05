9bbbf443d11649d965efb516137ce76a1cc5847b
Richard Allen <richard2891@gmail.com>
2019-10-04 22:38:11 -0400

    Add Commenting to DatabaseInteractions

--- DatabaseInteractions.py
+++ DatabaseInteractions.py
@@ -1,6 +1,7 @@
 from DatabaseModels import PlayDataset
 from DatabaseModels import db
 
+
 def update_database(play):
     playdataset = PlayDataset.query.order_by(PlayDataset.date.desc()).limit(1).all()
     if play.date > playdataset.date:
@@ -8,18 +9,27 @@ def update_database(play):
     else:
         return False
 
+
 def update_data(plays):
-    PlayDataset.query.order_by(PlayDataset.date.desc()).limit(1).all()
+    lates_play = PlayDataset.query.order_by(PlayDataset.date.desc()).limit(1).all()
     for play in plays:
 
         PlayDataset.query.filter_py(id=play.id).first()
 
 def rebuild_database():
+    """
+    drops all current tables in the database and creates all the new tables.
+    :return:
+    """
     db.drop_all()
     db.create_all()
 
 
 def load_database():
+    """
+    loads all the data in the database into a list of PlaysXMLDataset
+    :return: a list of PlaysXMLDataset
+    """
     rtn = []
     query_data = db.session.query(PlayDataset).all()
     for data in query_data:
@@ -28,6 +38,11 @@ def load_database():
 
 
 def load_data_into_database(plays):
+    """
+    Takes a list of PlaysXMLDataset and puts it into the database. with out check if that record already exists.
+    :param plays: list of PlaysXMLDataset
+    :return: None
+    """
     for play in plays:
         playdataset = PlayDataset(xml=play)
         db.session.add(playdataset)
