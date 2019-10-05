32c21292a3ed47d4b135841ced80d4e85463e53e
Richard Allen <richard2891@gmail.com>
2019-10-04 22:07:27 -0400

    cleaned up the loading and unloading of the database dataset modules to PlayerXMLDataset modules.

--- Database.py
+++ Database.py
@@ -11,37 +11,15 @@ def rebuild_database():
 
 def load_database():
     rtn = []
-    all_datas = db.session.query(PlayDataset).all()
-    for data in all_datas:
-        rtn.append(data.plays_xml_dataset())
+    query_data = db.session.query(PlayDataset).all()
+    for data in query_data:
+        rtn.append(data.xml)
     return rtn
 
 
 def load_data_into_database(plays):
-    game_pass = {}
-    player_pass = {}
     for play in plays:
-        playdataset = PlayDataset(id=play.id, date=play.date, quantity=play.quantity, length=play.length,
-                                  incomplete=play.incomplete, nowinstats=play.nowinstats, location=play.location)
-
-        for player in play.players:
-            if player.name not in player_pass:
-                playerdataset = PlayerDataset(username=player.username, userid=player.userid, name=player.name)
-                db.session.add(playerdataset)
-                player_pass[player.name] = playerdataset
-            playersplaydataset = PlayersPlayDataset(playerdataset=player_pass[player.name],  playdataset=playdataset,
-                                                    startposition=player.startposition, colour=player.colour,
-                                                    score=player.score, new=player.new, rating=player.rating,
-                                                    won=player.won)
-            db.session.add(playersplaydataset)
-
-        if play.gameid not in game_pass:
-            gamedataset = GameDataset(id=play.gameid, name=play.game_name)
-            db.session.add(gamedataset)
-            game_pass[play.gameid] = gamedataset
-
-        playdataset.gamedataset = game_pass[play.gameid]
-
+        playdataset = PlayDataset(xml=play)
         db.session.add(playdataset)
         db.session.commit()
 
@@ -50,12 +28,31 @@ class GameDataset(db.Model):
     id = db.Column(db.Integer, unique=True, primary_key=True)
     name = db.Column(db.String(120))
 
+    @property
+    def xml(self):
+        return PlaysXMLDataset(id=self.id, name=self.name)
+
+    @xml.setter
+    def xml(self, val):
+        self.name = val.game_name
+        self.id = val.gameid
+
 
 class PlayerDataset(db.Model):
     id = db.Column(db.Integer, unique=True, primary_key=True)
     username = db.Column(db.String(120))
     userid = db.Column(db.Integer)
     name = db.Column(db.String(120), unique=True, nullable=False)
+
+    @property
+    def xml(self):
+        return PlayerXMLDataset(username=self.username, userid=self.userid, name=self.name)
+
+    @xml.setter
+    def xml(self, val):
+        self.username = val.username
+        self.userid = val.userid
+        self.name = val.name
 
 
 class PlayDataset(db.Model):
@@ -70,16 +67,32 @@ class PlayDataset(db.Model):
     gamedataset = db.relationship('GameDataset', backref=db.backref('play_dataset', lazy=True))  # One to One
     playersplaydataset = db.relationship('PlayersPlayDataset',
                                          backref=db.backref('play_dataset', lazy=True))  # One to Many
-
-    def plays_xml_dataset(self):
+    @property
+    def xml(self):
         plays = PlaysXMLDataset(id=self.id, date=self.date, quantity=self.quantity, length=self.length,
                                 incomplete=self.incomplete, nowinstats=self.nowinstats, location=self.location,
                                 game_name=self.gamedataset.name, gameid=self.gamedataset.id)
 
         for x in self.playersplaydataset:
-            plays.players.append(x.player_xml_dataset())
+            plays.players.append(x.xml)
 
         return plays
+
+    @xml.setter
+    def xml(self, val):
+        self.id = val.id
+        self.date = val.date
+        self.quantity = val.quantity
+        self.length = val.length
+        self.incomplete = val.incomplete
+        self.nowinstats = val.nowinstats
+        self.location = val.location
+        self.gamedataset = GameDataset.query.filter_by(id=val.gameid).first()
+        if self.gamedataset is None:
+            self.gamedataset = GameDataset(xml=val)
+        for player in val.players:
+            self.playersplaydataset.append(PlayersPlayDataset(xml=player))
+
 
 
 class PlayersPlayDataset(db.Model):
@@ -96,7 +109,28 @@ class PlayersPlayDataset(db.Model):
     rating = db.Column(db.String(40))
     won = db.Column(db.Boolean)
 
-    def player_xml_dataset(self):
+    @property
+    def xml(self):
         return PlayerXMLDataset(username=self.playerdataset.username, userid=self.playerdataset.userid,
                                 name=self.playerdataset.name, startposition=self.startposition, colour=self.colour,
                                 score=self.score, new=self.new, rating=self.rating, won=self.won)
+
+    @xml.setter
+    def xml(self, val):
+        self.startposition = val.startposition
+        self.colour = val.colour
+        self.score = val.score
+        self.new = val.new
+        self.rating = val.rating
+        self.won = val.won
+        self.playerdataset = PlayerDataset.query.filter_by(name=val.name).first()
+        if self.playerdataset is None:
+            self.playerdataset = PlayerDataset(xml=val)
+
+
+if __name__ == "__main__":
+    tmp = PlaysXMLDataset()
+    tmp.gameid = 99
+    tmp.game_name = "Hello World"
+
+    data = GameDataset(xml=tmp)
